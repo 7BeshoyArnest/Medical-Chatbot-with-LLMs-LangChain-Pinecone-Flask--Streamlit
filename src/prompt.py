db3d6a0a386a0aa03f7de9a080d9a1f895526d7e
Beshoy Arnest <beshoyarnest01@gmail.com>
2026-05-06 14:29:25 +0300

    apply full rag notebook then module the pipleline

---
+++ src/prompt.py
@@ -0,0 +1,10 @@
+system_prompt = (
+    "you are as medical assistant for question-answering tasks. "
+    "use the following retrieved documents to answer the question. "
+    "the question. If you don't know the answer, say you don't know. "
+    "don't know. Use three sentences at max to answer the question. "
+    "answer concise."
+    "\n\n"
+    "{context}"
+
+)
