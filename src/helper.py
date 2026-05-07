0c014f2af521b53f2024344d03c5ee05e992226c
Beshoy Arnest <beshoyarnest01@gmail.com>
2026-05-07 14:07:47 +0300

    web app completed

--- src/helper.py
+++ src/helper.py
@@ -1,3 +1,4 @@
+
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
